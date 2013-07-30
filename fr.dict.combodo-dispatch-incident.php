88cabdfec7e04f1d96151e06a757c201da6643b0
Denis Flaven <denis.flaven@combodo.com>
2013-07-30 15:28:09 +0000

    Add an intermediate "dispatched" state (i.e. assigned to a team but not to an agent) to Incident Tickets. This state lies between new and assigned. Also add a redispatched state in case of dispatching again, to make the proper TTO computation (and initial assignment date).
    SVN:trunk[2685]

---
+++ fr.dict.combodo-dispatch-incident.php
@@ -0,0 +1,7 @@
+<?php
+Dict::Add('FR FR', 'French', 'Français', array(
+	// Dictionary entries go here
+	'Class:Incident/Attribute:status/Value:dispatched' => 'Affecté',
+	'Class:Incident/Attribute:status/Value:redispatched' => 'Réaffecté',
+	'Class:Incident/Stimulus:ev_dispatch' => 'Affecter à une équipe',
+));
