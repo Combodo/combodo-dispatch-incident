263a15658e16a8f92b90e8e15b771ab953bee955
Romain Quetiez <romain.quetiez@combodo.com>
2014-11-19 10:39:03 +0000

    Adaptation aux dernières évolutions sur les action du lifecycle: utilisation des actions standards comme Reset, SetCurrentDate et ResolveChildTickets
    SVN:trunk[4075]

--- module.combodo-dispatch-incident.php
+++ module.combodo-dispatch-incident.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-dispatch-incident/1.1.0',
+	'combodo-dispatch-incident/1.1.1',
 	array(
 		// Identification
 		//
