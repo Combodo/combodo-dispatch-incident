25bc339a4f1574f786feeb80c358e38a22e9e53f
Vincent Dumas <vincent.dumas@combodo.com>
2017-10-02 14:14:06 +0000

    Full new DE dictionnary from itomig for Dispatch Incident
    SVN:trunk[6158]

--- de.dict.combodo-dispatch-incident.php
+++ de.dict.combodo-dispatch-incident.php
@@ -7,11 +7,11 @@
  */
 
 Dict::Add('DE DE', 'German', 'Deutsch', array(
-	// Dictionary entries go here
-	'Class:Incident/Attribute:status/Value:dispatched' => 'Zugewiesen',
-	'Class:Incident/Attribute:status/Value:redispatched' => 'Neu zugewiesen',
-	'Class:Incident/Stimulus:ev_dispatch' => 'An ein Team zugewiesen',
-	// Menu entry
-	'Menu:Incident:IncidentsDispatchedToMyTeams' => 'Incidents die meinem Team zugewiesen sind',
-	'Menu:Incident:IncidentsDispatchedToMyTeams+' => 'Offene Incidents, die meinem Team zugewiesen sind',
+		// Dictionary entries go here
+		'Class:Incident/Attribute:status/Value:dispatched' => 'Verteilt',
+		'Class:Incident/Attribute:status/Value:redispatched' => 'Erneut verteilt',
+		'Class:Incident/Stimulus:ev_dispatch' => 'An ein Team verteilen',
+		// Menu entry
+		'Menu:Incident:IncidentsDispatchedToMyTeams' => 'An mein Team verteilte Incidents',
+		'Menu:Incident:IncidentsDispatchedToMyTeams+' => 'Offene Incidents, die an mein Team verteilt worden sind',
 ));
