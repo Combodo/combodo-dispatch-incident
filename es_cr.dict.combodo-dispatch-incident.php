8325eb7dee2216a11958772514d9ffe63fe751d6
Guillaume Lajarige <guillaume.lajarige@combodo.com>
2018-02-16 10:15:22 +0000

    combodo-dispatch-incident: Update spanish translations (Thanks to Miguel Turrubiates!)
    SVN:trunk[6497]

---
+++ es_cr.dict.combodo-dispatch-incident.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 Combodo SARL
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('ES CR', 'Spanish', 'Español, Castellano', array(
+	// Dictionary entries go here
+	'Class:Incident/Attribute:status/Value:dispatched' => 'Despachado',
+	'Class:Incident/Attribute:status/Value:redispatched' => 'Redespachado',
+	'Class:Incident/Stimulus:ev_dispatch' => 'Despachar a un equipo',
+	// Menu entry
+	'Menu:Incident:IncidentsDispatchedToMyTeams' => 'Incidentes despachados a mis equipos',	
+	'Menu:Incident:IncidentsDispatchedToMyTeams+' => 'Incidentes Abiertos despachados a uno de mis equipos',	
+));
