26500b149b3198f3f243d00b7708ffc7ddbdc882
Stephen Abello <stephen.abello@combodo.com>
2018-04-30 13:28:16 +0000

    combodo-dispatch-incident: updated german translation and created tag for v1.1.4 release.
    SVN:trunk[6603]

--- de.dict.combodo-dispatch-incident.php
+++ de.dict.combodo-dispatch-incident.php
@@ -8,10 +8,10 @@
 
 Dict::Add('DE DE', 'German', 'Deutsch', array(
 		// Dictionary entries go here
-		'Class:Incident/Attribute:status/Value:dispatched' => 'Verteilt',
-		'Class:Incident/Attribute:status/Value:redispatched' => 'Erneut verteilt',
-		'Class:Incident/Stimulus:ev_dispatch' => 'An ein Team verteilen',
+		'Class:Incident/Attribute:status/Value:dispatched' => 'Zugewiesen',
+		'Class:Incident/Attribute:status/Value:redispatched' => 'Erneut zugewiesen',
+		'Class:Incident/Stimulus:ev_dispatch' => 'Einem Team zuweisen',
 		// Menu entry
-		'Menu:Incident:IncidentsDispatchedToMyTeams' => 'An mein Team verteilte Incidents',
-		'Menu:Incident:IncidentsDispatchedToMyTeams+' => 'Offene Incidents, die an mein Team verteilt worden sind',
+		'Menu:Incident:IncidentsDispatchedToMyTeams' => 'An mein Team zugewiesene Incidents',
+		'Menu:Incident:IncidentsDispatchedToMyTeams+' => 'Offene Incidents, die an mein Team zugewiesen wurden',
 ));
