bfcb249519b18f7e69aa9c9be0b61508c3e892c1
Vincent Dumas <vincent.dumas@combodo.com>
2018-08-08 09:29:21 +0000

    German Translation: typos (UserRequest #18425)
    SVN:trunk[6988]

--- de.dict.combodo-dispatch-incident.php
+++ de.dict.combodo-dispatch-incident.php
@@ -8,8 +8,8 @@
 
 Dict::Add('DE DE', 'German', 'Deutsch', array(
 		// Dictionary entries go here
-		'Class:Incident/Attribute:status/Value:dispatched' => 'Zugewiesen',
-		'Class:Incident/Attribute:status/Value:redispatched' => 'Erneut zugewiesen',
+		'Class:Incident/Attribute:status/Value:dispatched' => 'Verteilt',
+		'Class:Incident/Attribute:status/Value:redispatched' => 'Erneut verteilt',
 		'Class:Incident/Stimulus:ev_dispatch' => 'Einem Team zuweisen',
 		// Menu entry
 		'Menu:Incident:IncidentsDispatchedToMyTeams' => 'An mein Team zugewiesene Incidents',
