fc192a1cf6487ee4b82046ab02ff579cbecedca8
Pierre Goiffon <pierre.goiffon@combodo.com>
2018-12-10 18:21:39 +0100

    Update l10n files

--- ru.dict.combodo-dispatch-incident.php
+++ ru.dict.combodo-dispatch-incident.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Localized data
  *
@@ -8,13 +7,12 @@
  * @link        https://github.com/itop-itsm-ru/itop-rus
  * @license     http://www.opensource.org/licenses/gpl-3.0.html LGPL
  */
-
 Dict::Add('RU RU', 'Russian', 'Русский', array(
-        // Dictionary entries go here
-        'Class:Incident/Attribute:status/Value:dispatched' => 'Направлен',
-        'Class:Incident/Attribute:status/Value:redispatched' => 'Перенаправлен',
-        'Class:Incident/Stimulus:ev_dispatch' => 'Направить в команду',
-        // Menu entry
-        'Menu:Incident:IncidentsDispatchedToMyTeams' => 'Направленные в мои команды',
-        'Menu:Incident:IncidentsDispatchedToMyTeams+' => 'Открытые инциденты, направленные в мои команды'
+	// Dictionary entries go here
+	'Class:Incident/Attribute:status/Value:dispatched' => 'Направлен',
+	'Class:Incident/Attribute:status/Value:redispatched' => 'Перенаправлен',
+	'Class:Incident/Stimulus:ev_dispatch' => 'Направить в команду',
+	// Menu entry
+	'Menu:Incident:IncidentsDispatchedToMyTeams' => 'Направленные в мои команды',	
+	'Menu:Incident:IncidentsDispatchedToMyTeams+' => 'Открытые инциденты, направленные в мои команды',	
 ));
